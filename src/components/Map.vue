e8f2e5758115d21c2643494c0b8b4b2fc85698a8
pia0y1 <1804507673@qq.com>
2023-02-08 23:15:55 +0800

    map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,61 +1,36 @@
 <template>
-  <div class="bmap" id="container"></div>
-  <div></div>
+  <div id="container"></div>
 </template>
 
 <script lang="ts">
-import { useStore } from 'vuex'
-// import { ref } from 'vue'
-export default {
-  name: 'BmapDemo',
-  mounted() {
-    const store = useStore()
-    var map = new window.BMap.Map('container')
-    var point = new window.BMap.Point(
-      store.state.record.longitude,//这里本人项目中可以有相关store数据，建议从自己项目出发进行修改
-      store.state.record.latitude
-    )
-    map.centerAndZoom(point, 18)
-    map.enableScrollWheelZoom(true)
-    var label = new window.BMap.Label('疲劳地点', {
-      position: point, // 设置标注的地理位置
-      offset: new window.BMap.Size(0, 0) // 设置标注的偏移量
-    })
-    // 添加标签
-    map.addOverlay(label) // 将标注添加到地图中
-    label.setStyle({
-      fontSize: '32px',
-      color: 'red'
-    })
+import { defineComponent, onMounted } from 'vue'
+export default defineComponent({
+  props: { },
+  setup(props, { emit }) {
+    const BMap = (window as any).BMap
 
-    var marker = new window.BMap.Marker(point) // 创建标注
-    map.addOverlay(marker) // 将标注添加到地图中
-    var scaleCtrl = new window.BMap.ScaleControl() // 添加比例尺控件
-    map.addControl(scaleCtrl)
-    var zoomCtrl = new window.BMap.ZoomControl() // 添加缩放控件
-    map.addControl(zoomCtrl)
-    var cityCtrl = new window.BMap.CityListControl() // 添加城市列表控件
-    map.addControl(cityCtrl)
+    onMounted(() => {
+      var map = new BMap.Map('container')
+      // var point = new BMap.Point(props.longitude, props.latitude)
+        var point = new BMap.Point(116.46, 39.92)
+      //   console.log(point)
+
+      map.centerAndZoom(point, 15)
+      map.enableScrollWheelZoom(true)
+      map.addEventListener('click', function (e: any) {
+        //   let a = JSON.parse(e)
+        console.log('点击的经纬度：' + e.point.lng + ',' + e.point.lat) //
+        emit('update:longitude', e.point.lng)
+        emit('update:latitude', e.point.lat)
+      })
+    })
   },
-  setup() {
-    // const store = useStore()
-    // let latitude = ref('')
-    // let longitude = ref('')
-    // console.log(store.state.record.latitude)
-    // latitude.value = store.state.record.latitude
-    // longitude.value = store.state.record.longitude
-    // return {
-    //   latitude,
-    //   longitude
-    // }
-  }
-}
+})
 </script>
 
-<style scoped>
-.bmap {
-  width: 800px;
-  height: 600px;
-  border: 1px solid #000;
+<style>
+#container {
+  width: 100%;
+  height: 900px;
 }
 </style>
